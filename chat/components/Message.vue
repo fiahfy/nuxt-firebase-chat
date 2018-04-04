f47cf61842cf7df7358a77614cd5b287488538d8
fiahfy <d.fiahfy@gmail.com>
2018-04-04 09:24:34 +0900

    Update

--- chat/components/Message.vue
+++ chat/components/Message.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="message">
+  <md-content class="message">
     <md-avatar class="md-avatar-icon">A</md-avatar>
-    <p>{{ message.message }}</p>
-  </div>
+    <div class="container">
+      <div>
+        <span class="md-body-2">Ada Lovelace</span>
+        <span class="md-caption">{{ datetime }}</span>
+      </div>
+      <p class="md-body-1">{{ message.message }}</p>
+    </div>
+  </md-content>
 </template>
 
 <script>
@@ -12,6 +18,15 @@ export default {
       type: Object,
       required: true
     }
+  },
+  computed: {
+    datetime () {
+      const date = this.message.createdAt
+      if (!(date instanceof Date)) {
+        return ''
+      }
+      return `${('00' + date.getHours()).slice(-2)}:${('00' + date.getMinutes()).slice(-2)}`
+    }
   }
 }
 </script>
@@ -19,10 +34,17 @@ export default {
 <style scoped lang="scss">
 .message {
   display: flex;
-  p {
+  .container {
+    display: flex;
     flex: 1;
-    line-height: 40px;
+    flex-direction: column;
     margin-left: 15px;
+    div .md-caption {
+      margin-left: 10px;
+    }
+    p {
+      flex: 1;
+    }
   }
 }
 </style>
--- chat/pages/index.vue
+++ chat/pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <section>
     <div ref="message-container" class="message-container">
-      <div class="offset" />
-      <message v-for="message of messages.slice().reverse()" :key="message.id" :message="message" />
+      <div class="spinner" v-if="loading">
+        <md-progress-spinner md-mode="indeterminate" />
+      </div>
+      <message v-else v-for="message of messages.slice().reverse()" :key="message.id" :message="message" />
     </div>
     <md-field>
-      <md-textarea v-model="message" md-autogrow placeholder="Input message" @keydown="keydown"></md-textarea>
+      <md-textarea v-model="message" md-autogrow placeholder="Message..." @keydown="keydown"></md-textarea>
     </md-field>
   </section>
 </template>
@@ -19,23 +21,25 @@ export default {
   },
   data () {
     return {
+      loading: true,
       messages: [],
       message: ''
     }
   },
   async mounted () {
-    this.unsubscribe = this.$db.collection('messages').orderBy('created_at', 'desc').onSnapshot((snapshot) => {
-        console.log(snapshot)
-      let messages = []
-      snapshot.docChanges.forEach((change) => {
-        console.log(change.doc.data())
-        if (change.type === 'added') {
-          messages.push({
-            id: change.doc.id, ...change.doc.data()
-          })
-        }
-      })
-      this.messages = [...messages, ...this.messages]
+    this.unsubscribe = this.$db.collection('messages').orderBy('createdAt', 'desc').onSnapshot((snapshot) => {
+      this.loading = false
+      this.messages = [
+        ...snapshot.docChanges.reduce((carry, change) => {
+          if (change.type === 'added') {
+            carry.push({
+              id: change.doc.id, ...change.doc.data()
+            })
+          }
+          return carry
+        }, []),
+        ...this.messages
+      ]
       this.$nextTick(() => {
         const container = this.$refs['message-container']
         container.scrollTop = container.scrollHeight
@@ -47,7 +51,7 @@ export default {
   },
   methods: {
     async keydown (e) {
-      if (e.keyCode === 13 && e.shiftKey) {
+      if (e.keyCode === 13 && !e.shiftKey) {
         e.preventDefault()
         e.stopPropagation()
         if (!this.message.length) {
@@ -56,7 +60,7 @@ export default {
         try {
           await this.$db.collection('messages').add({
             message: this.message,
-            created_at: new Date
+            createdAt: new Date
           })
           this.message = ''
         } catch (e) {
@@ -79,6 +83,12 @@ section {
   overflow: auto;
   .message {
     margin: 15px 0;
+  }
+  .spinner {
+    align-items: center;
+    display: flex;
+    height: 100%;
+    justify-content: center;
   }
 }
 </style>
