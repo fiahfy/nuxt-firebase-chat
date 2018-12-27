f00263229eaec28091d2990e59a74080ae575df0
fiahfy <d.fiahfy@gmail.com>
2018-12-27 10:05:19 +0900

    Update basic configs

--- src/chat/.eslintrc.js
+++ src/chat/.eslintrc.js
@@ -1,21 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'eslint:recommended',
+    'plugin:prettier/recommended',
     'plugin:vue/recommended'
   ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {}
+  rules: {
+    'vue/component-name-in-template-casing': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/singleline-html-element-content-newline': 'off'
+  }
 }
--- src/chat/nuxt.config.js
+++ src/chat/nuxt.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Build configuration
+   */
+  build: {},
+
+  /*
+   ** Headers of the page
+   */
   head: {
     title: 'chat',
     meta: [
@@ -9,54 +14,33 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/chat/favicon.ico' },
-      { rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/chat/favicon.ico' }]
   },
+
   /*
-  ** Customize the progress bar color
-  */
+   ** Customize the progress bar color
+   */
   loading: { color: '#3B8070' },
+
   /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  },
-  /*
-  ** Mode
-  */
+   ** Mode
+   */
   mode: 'spa',
+
   /*
-  ** Router configuration
-  */
+   ** Modules
+   */
+  modules: ['@nuxtjs/dotenv', '@nuxtjs/vuetify'],
+
+  /*
+   ** Plugins
+   */
+  plugins: ['~/plugins/firebase'],
+
+  /*
+   ** Router configuration
+   */
   router: {
     base: '/chat/'
-  },
-  /*
-  ** Plugins
-  */
-  plugins: [
-    '~/plugins/firebase'
-  ],
-  /*
-  ** Modules
-  */
-  modules: [
-    '@nuxtjs/dotenv',
-    '@nuxtjs/vuetify'
-  ]
+  }
 }
